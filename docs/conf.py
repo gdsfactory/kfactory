b9f87dbd3c54c36a219f5b609da4be7e28ad7e13
Sebastian Goeldi <sgoeldi@psiquantum.com>
2023-02-07 00:54:54 -0800

    Bump version: 0.1.3 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "kfactory"
-version = "0.1.3"
+version = "0.2.0"
 copyright = "2022"
 # author = "kfactory"
 
--- src/kfactory/__init__.py
+++ src/kfactory/__init__.py
@@ -25,7 +25,7 @@ from .utils import show
 from . import port
 
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 
 __all__ = [
