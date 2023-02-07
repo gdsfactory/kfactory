0f3116205151ecb0930f09bd61489eb1e5073d1f
Sourcery AI <>
2023-02-07 08:51:42 +0000

    'Refactored by Sourcery'

--- src/kfactory/utils/geo.py
+++ src/kfactory/utils/geo.py
@@ -187,12 +187,12 @@ def extrude_path(
     layer_list = {layer: LayerSection(sections=[Section(d_max=0)])}
     if enclosure is not None:
         if layer not in enclosure.layer_sections:
-            layer_list.update(enclosure.layer_sections)
+            layer_list |= enclosure.layer_sections
         else:
             ls = layer_list[layer].sections.copy()
             layer_list = enclosure.layer_sections.copy()
             layer_list[layer] = LayerSection(
-                sections=[sec for sec in layer_list[layer].sections] + [ls]
+                sections=list(layer_list[layer].sections) + [ls]
             )
 
     for layer, layer_sec in layer_list.items():
@@ -325,7 +325,7 @@ def extrude_path_dynamic(
             ls = layer_list[layer].sections.copy()
             layer_list = enclosure.layer_sections.copy()
             layer_list[layer] = LayerSection(
-                sections=[sec for sec in layer_list[layer].sections] + [ls]
+                sections=list(layer_list[layer].sections) + [ls]
             )
     if is_callable_widths(widths):
         for layer, layer_sec in layer_list.items():
@@ -575,13 +575,9 @@ class Enclosure(BaseModel):
 
         if ref is None:
             raise ValueError(
-                f"The enclosure doesn't have  a reference `main_layer` defined. Therefore the layer must be defined in calls"
+                "The enclosure doesn't have  a reference `main_layer` defined. Therefore the layer must be defined in calls"
             )
-        if isinstance(ref, int):
-            r = kdb.Region(c.begin_shapes_rec(ref))
-        else:
-            r = ref.dup()
-
+        r = kdb.Region(c.begin_shapes_rec(ref)) if isinstance(ref, int) else ref.dup()
         r.merge()
 
         for layer, layersec in self.layer_sections.items():
@@ -613,10 +609,9 @@ class Enclosure(BaseModel):
             reg_max.size(d_max)
             if d_min is None:
                 return reg_max
-            else:
-                reg_min = kdb.Region(_ref)
-                reg_min.size(d_min)
-                return reg_max - reg_min
+            reg_min = kdb.Region(_ref)
+            reg_min.size(d_min)
+            return reg_max - reg_min
 
         self.apply_custom(c, bbox_reg)
 
@@ -645,7 +640,7 @@ class Enclosure(BaseModel):
     ) -> None:
         if main_layer is None:
             raise ValueError(
-                f"The enclosure doesn't have  a reference `main_layer` defined. Therefore the layer must be defined in calls"
+                "The enclosure doesn't have  a reference `main_layer` defined. Therefore the layer must be defined in calls"
             )
         extrude_path(target=c, layer=main_layer, path=path, width=width, enclosure=self)
 
@@ -658,7 +653,7 @@ class Enclosure(BaseModel):
     ) -> None:
         if main_layer is None:
             raise ValueError(
-                f"The enclosure doesn't have  a reference `main_layer` defined. Therefore the layer must be defined in calls"
+                "The enclosure doesn't have  a reference `main_layer` defined. Therefore the layer must be defined in calls"
             )
         extrude_path_dynamic(
             target=c, layer=main_layer, path=path, widths=widths, enclosure=self
