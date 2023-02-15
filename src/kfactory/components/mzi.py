d108c7662b73aed56826050f2fa79d5f9d5bb658
Hsuan-Tung Peng <hpeng@psiquantum.com>
2023-02-15 15:35:49 -0800

    adding typing in connect_sequence

---
+++ src/kfactory/components/mzi.py
@@ -0,0 +1,13 @@
+from typing import Any, Callable, Optional, Sequence
+
+import kfactory as kf
+from kfactory.pcells.euler import bend_euler
+from kfactory.pcells.taper import taper
+from kfactory.pcells.waveguide import waveguide
+
+### To do:
+# from kfactory.pcells.DCs import *
+# from kfactory.pcells.heater import wg_heater_connected
+# from kfactory.tech.layers import LAYER (create some default layer for users)
+# from kfactory.utils.enclosures import LAYER_ENC, WG_STANDARD
+# from kfactory.utils.connection import connect_sequence
---
+++ src/kfactory/utils/connection.py
@@ -0,0 +1,57 @@
+import kfactory as kf
+
+
+@kf.autocell
+def connect_sequence(
+    seq: list[tuple[kf.KCell, str, str]],
+    port_map: tuple[tuple[str, int, str]],
+    input_port_name: str = "W0",
+    output_port_name: str = "E0",
+) -> kf.KCell:
+    """
+    Args:
+        seq: is in the format of (component function name, component input port name, component output port name, flip along input angle or not)
+        e.g. (DC(), "W0", "E0", False) -> adding a DC KCell, DC(), without flipping/mirroring the device
+
+        port_map: allows you to define extra ports in the connected component sequence,
+        and it is in the format of (port name in final KCell, component's order in seq, port name in the component)
+        e.g. ("W2", 2, "W1") -> add a port "W2" in final KCell, which is the port "W1" of 2nd component in the connected sequence
+
+        input_port_name: the input port of the connected sequence (the input port of 1st component); default port name is "W0"
+
+        output_port_name: the output port of the connected sequence (the output port of the last component); default port name is "W0"
+
+        Remark: the reason that seq and port_map are all tuples is because it will be hasbable thereby can be decorated by kf.autocell
+
+    Return:
+        A KCell that connected the given sequence of components. The input/output port of this KCell is given by the input port of 1st component and output port of last component, and their port names match with the args.
+        This KCell will have extra ports if defined in port_map.
+    """
+
+    c = kf.KCell()
+    beg = c << seq[0][0]  # ()
+    port1 = beg.ports[seq[0][1]]
+    c.add_port(name=input_port_name, port=port1)
+    conn_port = beg.ports[seq[0][2]]
+    count = 1
+    inst_list = [beg]
+    for dev_spec in seq[1:]:
+        kcell = dev_spec[0]  # ()
+        p_1 = dev_spec[1]
+        p_2 = dev_spec[2]
+        if len(dev_spec) >= 4:
+            inv = dev_spec[3]  # type: ignore[misc]
+        else:
+            inv = False
+        inst = c << kcell
+        inst.connect(p_1, conn_port, mirror=inv)
+        conn_port = inst.ports[p_2]
+        count += 1
+        inst_list.append(inst)
+        if count == len(seq):
+            c.add_port(name=output_port_name, port=conn_port)
+
+    for (name, alias, alias_port_name) in port_map:
+        c.add_port(name=name, port=inst_list[alias].ports[alias_port_name])
+
+    return c
