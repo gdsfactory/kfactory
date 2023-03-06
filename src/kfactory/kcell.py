2219ca77e12c0dc29fbdbb61771d92c1250e2e91
sebastian-goeldi <44963764+sebastian-goeldi@users.noreply.github.com>
2023-03-06 21:54:06 +0100

    Fix Bezier and dbu cells (#57)
    * move tech to generic_tech
    
    * fix bezier and dbu cells
    
    * Bump version: 0.4.4 → 0.4.5
    
    * Bump version: 0.4.5 → 0.4.6

--- src/kfactory/kcell.py
+++ src/kfactory/kcell.py
@@ -1677,7 +1677,10 @@ class KCell(ABCKCell[Port]):
         show(self)
 
     def _ipython_display_(self) -> None:
-        from IPython.display import Image, display  # type: ignore[attr-defined]
+        # try:
+        #     import kweb
+        # except ImportError:
+        from IPython.display import Image, display  # type: ignore
 
         lv = lay.LayoutView()
         l = lv.create_layout(False)
@@ -1690,14 +1693,16 @@ class KCell(ABCKCell[Port]):
         kc.ports = self.ports.copy()
         kc.draw_ports()
 
-        lv.active_cellview().layout().assign(klib_dup)
+        lv.show_layout(klib_dup, False)
+
+        # lv.active_cellview().layout().assign(klib_dup)
         lv.add_missing_layers()
         lv.active_cellview().cell = kc
         lv.max_hier()
         lv.zoom_fit()
         pb = lv.get_pixels(800, 800)
         # dup.klib.delete_cell(dup.cell_index())
-        display(Image(data=pb.to_png_data(), format="png"))  # type: ignore[no-untyped-call]
+        display(Image(data=pb.to_png_data(), format="png"))
 
 
 class CplxKCell(ABCKCell[DCplxPort]):
