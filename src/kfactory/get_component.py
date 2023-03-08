2cd58cf2dfe4452d1d5379b3f15d0f075af78978
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-03-07 17:56:11 -0800

    Create get_component.py

---
+++ src/kfactory/get_component.py
@@ -0,0 +1,31 @@
+from .kcell import KCell
+from .types import ComponentSpec
+
+
+def get_component(component_spec: ComponentSpec, **kwargs) -> KCell:
+    if isinstance(component_spec, str):
+        from .pcells import pcells
+        from .components import components
+
+        try:
+            return pcells[component_spec](**kwargs)
+        except KeyError:
+            try:
+                return components[component_spec](**kwargs)
+            except KeyError:
+                raise ValueError(
+                    f"Invalid component_spec: {component_spec}. "
+                    "Must be a valid pcell or component name."
+                )
+    elif isinstance(component_spec, KCell):
+        return component_spec
+    elif isinstance(component_spec, dict):
+        component_spec.update(kwargs)
+        return KCell(**component_spec)
+    elif callable(component_spec):
+        return component_spec(**kwargs)
+    else:
+        raise ValueError(
+            f"Invalid component_spec: {component_spec}. "
+            "Must be a string, a dict, a KCell, or a callable."
+        )
