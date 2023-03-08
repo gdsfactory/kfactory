7700510d0336b70a46fac6f124a6922bc23f3ee9
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-03-07 18:01:24 -0800

    Update __init__.py

--- src/kfactory/pcells/__init__.py
+++ src/kfactory/pcells/__init__.py
@@ -1,2 +1,25 @@
 # flake8: noqa
-from . import bezier, circular, dbu, euler, taper, waveguide
+from .bezier import bend_s
+from .circular import bend_circular
+from .euler import bend_euler, bend_s_euler
+from .taper import taper
+from .waveguide import waveguide
+
+
+__all__ = [
+    "bend_s",
+    "bend_circular",
+    "bend_euler",
+    "bend_s_euler",
+    "taper",
+    "waveguide",
+]
+
+pcells = {
+    "bend_s": bend_s,
+    "bend_circular": bend_circular,
+    "bend_euler": bend_euler,
+    "bend_s_euler": bend_s_euler,
+    "taper": taper,
+    "waveguide": waveguide,
+}
