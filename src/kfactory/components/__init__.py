2ed0739d154a40be65f2c777e182bd0f1f148b6c
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-03-20 17:26:11 -0700

    Update __init__.py

--- src/kfactory/components/__init__.py
+++ src/kfactory/components/__init__.py
@@ -1,8 +1,15 @@
-from .DCs import coupler
+from .DCs import coupler, straight_coupler
+from .mzi import mzi
 
 
-__all__ = ["coupler"]
+__all__ = [
+    "coupler",
+    "mzi",
+    "straight_coupler",
+]
 
 components = {
     "coupler": coupler,
+    "mzi": mzi,
+    "straight_coupler": straight_coupler,
 }
