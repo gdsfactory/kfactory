5e0c5259d6f17cc410ac5dca05fbdd0330607a6b
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-03-20 17:29:58 -0700

    Update types.py

--- src/kfactory/types.py
+++ src/kfactory/types.py
@@ -1,9 +1,9 @@
+from pathlib import Path
 from typing import Any, Dict, Type, Union, Callable
 
 from .kcell import KCell
 
 CellSpec = Union[Type[str], KCell | Type[Dict[str, Any]]]
 ComponentFactory = Callable[..., KCell]
-ComponentSpec = Union[
-    str, ComponentFactory, KCell, Dict[str, Any]
-]
+ComponentSpec = Union[str, ComponentFactory, KCell, Dict[str, Any]]
+PathType = Union[str, Path]
