371aaa5b8852fa5a7a9b699a5fda6e6d1875b876
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-03-25 13:20:52 -0700

    Update __init__.py

--- src/kfactory/__init__.py
+++ src/kfactory/__init__.py
@@ -22,7 +22,7 @@ from .kcell import (
     LayerEnum,
     show,
 )
-from . import pcells, placer, routing, utils, port, components
+from . import pcells, placer, routing, utils, port
 from .config import logger
 from .get_component import get_component
 
