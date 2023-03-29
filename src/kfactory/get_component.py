2c88053aab7b6e0e5868d1476b8a01a322201fc8
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-03-29 10:56:19 -0700

    Update get_component.py

--- src/kfactory/get_component.py
+++ src/kfactory/get_component.py
@@ -1,15 +1,15 @@
 from typing import Any
 
 import kfactory as kf
+
 from .kcell import KCell
 from .typs import ComponentSpec
 
 
 def get_component(component_spec: ComponentSpec, **kwargs: Any) -> KCell:
     if isinstance(component_spec, str):
-
         try:
-            return getattr(kf.pcells, component_spec)(**kwargs) # type: ignore
+            return getattr(kf.pcells, component_spec)(**kwargs)  # type: ignore
         except KeyError:
             raise ValueError(
                 f"Invalid component_spec: {component_spec}. "
