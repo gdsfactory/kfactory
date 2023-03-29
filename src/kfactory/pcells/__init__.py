29e6b84228877781b0b8e62cacd3274a7bb8e9ad
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-03-28 17:44:21 -0700

    Update __init__.py

--- src/kfactory/pcells/__init__.py
+++ src/kfactory/pcells/__init__.py
@@ -5,9 +5,11 @@ import kfactory as kf
 
 from .bezier import bend_s
 from .circular import bend_circular
+from .DCs import coupler, straight_coupler
 from .euler import bend_euler, bend_s_euler
-from .taper import taper as taper_function
-from .waveguide import waveguide as wg
+from .mzi import mzi
+from .taper import taper
+from .waveguide import waveguide
 
 __all__ = [
     "bend_s",
@@ -18,11 +20,15 @@ __all__ = [
     "waveguide",
 ]
 
-pcells: dict[str, Callable[..., kf.kcell.KCell]] = {  # type: ignore
+pcells: dict[str, Callable[..., kf.kcell.KCell]] = {
     "bend_s": bend_s,
     "bend_circular": bend_circular,
     "bend_euler": bend_euler,
     "bend_s_euler": bend_s_euler,
-    "taper": taper_function,
-    "waveguide": wg,
+    "coupler": coupler,
+    "straight_coupler": straight_coupler,
+    "mzi": mzi,
+    "taper": taper,
+    "waveguide": waveguide,
+    
 }
