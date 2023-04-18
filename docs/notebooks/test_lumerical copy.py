002722718139dd1788b35bc92f27319f4573e29b
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-04-18 11:35:28 -0700

    Update test_lumerical copy.py

--- docs/notebooks/test_lumerical copy.py
+++ docs/notebooks/test_lumerical copy.py
@@ -8,16 +8,15 @@ from kfactory.pcells.waveguide import waveguide
 from kfactory.simulation.write_sparameters_lumerical import plot_sparameters_lumerical
 
 c1 = coupler()
-c1.insts[0].cell.info["sparameters"] = "C:\\Users\\schandrasekar\\Downloads\\smat_Sbend_900-1100x350_gap710_r60_h3_wlcombined2_3Dopt (1).dat"
-c1.insts[2].cell.info["sparameters"] = "C:\\Users\\schandrasekar\\Downloads\\smat_Sbend_900-1100x350_gap710_r60_h3_wlcombined2_3Dopt (1).dat"
-c1.insts[1].cell.info["sparameters"] = "C:\\Users\\schandrasekar\\Downloads\\MODE_3D_SiNWG_w1100x350_g840_BB (1).ldf"
-
+c1.insts[0].cell.info["sparameters"] = "sbend sparameter.dat"
+c1.insts[2].cell.info["sparameters"] = "sbend sparameters.dat"
+c1.insts[1].cell.info["sparameters"] = "straight coupler.ldf"
+kf.kcell.rename_clockwise(c1.ports, prefix="port")
 
 bend = bend_euler(0.5, 5, 0)
-bend.info["sparameters"] = "C:\\Users\\schandrasekar\\Downloads\\MODE_3D_SiNWG_r900x350_g710_BB_300K (1).ldf"
+bend.info["sparameters"] = "bend euler.ldf"
 
-
-mzi1 = mzi(splitter=c1, bend_component=bend, width=0.5)
+mzi1 = mzi(splitter=c1, bend_component=bend, width=0.5, port_e0_splitter="port4", port_e1_splitter="port3")
 
 mzi1.show()
 
