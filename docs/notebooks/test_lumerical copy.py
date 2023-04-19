16154a4d0ba1bb45539068dea23eb1c786e5f05c
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-04-19 12:19:52 -0700

    Update test_lumerical copy.py

--- docs/notebooks/test_lumerical copy.py
+++ docs/notebooks/test_lumerical copy.py
@@ -18,6 +18,10 @@ bend.info["sparameters"] = "bend euler.ldf"
 
 mzi1 = mzi(splitter=c1, bend_component=bend, width=0.5, port_e0_splitter="port4", port_e1_splitter="port3")
 
+for inst in mzi1.insts:
+    if "sparameters" in inst.cell.info.keys():
+        continue
+    inst.cell.info["sparameters"] = "straight sparameters.dat"
 mzi1.show()
 
 plot_sparameters_lumerical(mzi1)
