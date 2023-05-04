0c5adaf5a63b16e29b848ed78fd5e86cc2dec7d5
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2023-05-04 15:11:26 -0700

    Update test_pdk.py

--- tests/test_pdk.py
+++ tests/test_pdk.py
@@ -11,6 +11,10 @@ def test_get_enclosure(pdk: kf.pdk.Pdk):
     pdk.get_enclosure("wg")
 
 
+def test_cell_factory(pdk: kf.pdk.Pdk):
+    assert isinstance(pdk.cell_factories.taper(2, 1, 10, 0), kf.KCell)
+
+
 def test_get_layer(pdk: kf.pdk.Pdk, LAYER: kf.LayerEnum):
     assert pdk.get_layer((1, 0)) == LAYER.WG
     assert pdk.get_layer(0) == LAYER.WG
