39718f1ea16181f8b302178ea576a66e03c34e7b
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2023-05-22 22:32:45 +0200

    conf settings one underscore instead of two

--- src/kfactory/conf.py
+++ src/kfactory/conf.py
@@ -111,7 +111,7 @@ class Settings(BaseSettings):
         arbitrary_types_allowed = True
         fields = {"logger": {"exclude": True}}
         env_prefix = "kfactory_"
-        env_nested_delimiter = "__"
+        env_nested_delimiter = "_"
 
 
 config = Settings()
