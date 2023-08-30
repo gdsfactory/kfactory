3b6a9a7f72a08f101aa5f96409d48ad3bd8578d7
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2023-08-30 15:38:24 +0200

    remove custom submodule

--- src/kfactory/custom/dbu/__init__.py
+++
@@ -1,4 +0,0 @@
-from .straight import custom_straight
-from .taper import custom_taper
-
-__all__ = ["custom_straight", "custom_taper"]
--- src/kfactory/custom/test.py
+++
@@ -1,8 +0,0 @@
-import kfactory as kf
-import kfactory.custom as custom
-
-kcl = kf.KCLayout("Custom")
-
-s = custom.custom_straight(kcl)
-
-s(width=5, length=10, c=105)
