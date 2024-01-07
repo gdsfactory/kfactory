e01420a491cc24438f0b71cb6d4c256158865719
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-01-08 02:02:09 +0900

    add x,y center for Cell and UMKCell

--- src/kfactory/kcell.py
+++ src/kfactory/kcell.py
@@ -1364,6 +1364,16 @@ class KCell:
                 )
 
     @property
+    def x(self) -> int:
+        """Returns the x-coordinate of the center of the bounding box."""
+        return self._kdb_cell.bbox().center().x
+
+    @property
+    def y(self) -> int:
+        """Returns the y-coordinate of the center of the bounding box."""
+        return self._kdb_cell.bbox().center().y
+
+    @property
     def xmin(self) -> int:
         """Returns the x-coordinate of the left edge of the bounding box."""
         return self._kdb_cell.bbox().left
@@ -2966,6 +2976,22 @@ class UMKCell:
     def ysize(self) -> float:
         """Returns the height of the bounding box."""
         return self.parent._kdb_cell.dbbox().height()
+
+    @property
+    def x(self) -> float:
+        """X coordinate of the port in um."""
+        return self.parent._kdb_cell.dbbox().center().x
+
+    @property
+    def y(self) -> float:
+        """Y coordinate of the port in um."""
+        return self.parent._kdb_cell.dbbox().center().y
+
+    @property
+    def center(self) -> tuple[float, float]:
+        """Coordinate of the port in um."""
+        vec = self.parent._kdb_cell.dbbox().center()
+        return (vec.x, vec.y)
 
     @overload
     def create_inst(
