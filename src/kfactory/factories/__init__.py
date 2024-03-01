cd9189137f04623ee553f8e78b406581321c3762
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2024-03-01 12:54:10 +0100

    move classes to generate virtual cell functions to factories and make them functions

--- src/kfactory/factories/__init__.py
+++ src/kfactory/factories/__init__.py
@@ -1,2 +1,4 @@
-# flake8: noqa
-from . import bezier, circular, euler, straight, taper
+"""Factories for generating functions which can produce KCells or VKCells."""
+from . import bezier, circular, euler, straight, taper, virtual
+
+__all__ = ["bezier", "circular", "euler", "straight", "taper", "virtual"]
---
+++ src/kfactory/factories/virtual/__init__.py
@@ -0,0 +1,8 @@
+"""Virtual cell factories for kfactory.
+
+These are used for cases where any-angle placement is necessary,
+for example in all-angle routing.
+"""
+from . import circular, euler, straight, utils
+
+__all__ = ["circular", "euler", "straight", "utils"]
