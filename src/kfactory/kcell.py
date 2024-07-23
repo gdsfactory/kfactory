b00fd111884cbe185338b998bc68afe1dcdb90b7
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2024-07-23 16:52:49 +0200

    fix the other case of model_dump

--- src/kfactory/kcell.py
+++ src/kfactory/kcell.py
@@ -6922,7 +6922,7 @@ class Ports:
                     kcl=self.kcl,
                     name=name or port.name,
                     dcplx_trans=port.dcplx_trans,
-                    info=_port.info.model_dump(),
+                    info=port.info.model_dump(),
                     dwidth=port.dwidth,
                     layer=self.kcl.layers(_l) if _l in self.kcl.layers else _l,  # type: ignore[operator, call-arg]
                 )
