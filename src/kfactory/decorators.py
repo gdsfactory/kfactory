460540a263307f4e5013752ba6f09e7d315e1093
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2024-08-19 16:21:20 +0200

    fix new decorator typevar

--- src/kfactory/decorators.py
+++ src/kfactory/decorators.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Iterable
-from typing import TYPE_CHECKING, Any, Protocol, TypedDict, TypeVar, overload
+from typing import TYPE_CHECKING, Any, Protocol, TypedDict, overload
 
 from typing_extensions import Unpack
 
@@ -19,9 +19,6 @@ if TYPE_CHECKING:
         KCLayout,
         MetaData,
     )
-
-
-K = TypeVar("K", bound=KCell)
 
 
 class ModuleCellKWargs(TypedDict, total=False):
@@ -68,7 +65,7 @@ class KCellDecorator(Protocol):
 
     def __call__(
         self, **kwargs: Unpack[KCellDecoratorKWargs]
-    ) -> Callable[[KCellFunc[KCellParams, K]], KCellFunc[KCellParams, K]]:
+    ) -> Callable[[KCellFunc[KCellParams, KC]], KCellFunc[KCellParams, KC]]:
         """__call__ implementation."""
 
 
@@ -77,7 +74,7 @@ class ModuleDecorator(Protocol):
 
     def __call__(
         self, /, **kwargs: Unpack[ModuleCellKWargs]
-    ) -> Callable[[KCellFunc[KCellParams, K]], KCellFunc[KCellParams, K]]:
+    ) -> Callable[[KCellFunc[KCellParams, KC]], KCellFunc[KCellParams, KC]]:
         """__call__ implementation."""
 
 
@@ -85,15 +82,15 @@ def _module_cell(
     cell_decorator: KCellDecorator,
     /,
     **kwargs: Unpack[ModuleCellKWargs],
-) -> Callable[[KCellFunc[KCellParams, K]], KCellFunc[KCellParams, K]]:
+) -> Callable[[KCellFunc[KCellParams, KC]], KCellFunc[KCellParams, KC]]:
     """Constructs the actual decorator.
 
     Modifies the basename to the module if the module is not the main one.
     """
 
     def decorator_cell(
-        f: KCellFunc[KCellParams, K],
-    ) -> KCellFunc[KCellParams, K]:
+        f: KCellFunc[KCellParams, KC],
+    ) -> KCellFunc[KCellParams, KC]:
         mod = f.__module__
         basename = f.__name__ if mod == "__main" else f"{mod}_{f.__name__}"
         return cell_decorator(basename=basename, **kwargs)(f)
@@ -111,29 +108,29 @@ class Decorators:
     @overload
     def module_cell(
         self,
-        _func: KCellFunc[KCellParams, K],
+        _func: KCellFunc[KCellParams, KC],
         /,
-    ) -> KCellFunc[KCellParams, K]: ...
+    ) -> KCellFunc[KCellParams, KC]: ...
 
     @overload
     def module_cell(
         self, /, **kwargs: Unpack[ModuleCellKWargs]
-    ) -> Callable[[KCellFunc[KCellParams, K]], KCellFunc[KCellParams, K]]: ...
+    ) -> Callable[[KCellFunc[KCellParams, KC]], KCellFunc[KCellParams, KC]]: ...
 
     def module_cell(  # type: ignore[misc]
         self,
-        _func: KCellFunc[KCellParams, K] | None = None,
+        _func: KCellFunc[KCellParams, KC] | None = None,
         /,
         **kwargs: Unpack[ModuleCellKWargs],
     ) -> (
-        KCellFunc[KCellParams, K]
-        | Callable[[KCellFunc[KCellParams, K]], KCellFunc[KCellParams, K]]
+        KCellFunc[KCellParams, KC]
+        | Callable[[KCellFunc[KCellParams, KC]], KCellFunc[KCellParams, KC]]
     ):
         """Constructs the `@module_cell` decorator on KCLayout.decorators."""
 
         def mc(
             **kwargs: Unpack[ModuleCellKWargs],
-        ) -> Callable[[KCellFunc[KCellParams, K]], KCellFunc[KCellParams, K]]:
+        ) -> Callable[[KCellFunc[KCellParams, KC]], KCellFunc[KCellParams, KC]]:
             return _module_cell(self._cell, **kwargs)  # type: ignore[arg-type]
 
         return mc(**kwargs) if _func is None else mc(**kwargs)(_func)
