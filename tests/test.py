b79940b6854183e27b165deede42047c772ebc40
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2024-08-23 21:07:55 +0200

    remove unnecessary file

--- tests/test.py
+++
@@ -1,6 +0,0 @@
-import kfactory as kf
-
-kf.kcl.infos = kf.LayerInfos(WG=kf.kdb.LayerInfo(1, 0))
-kf.cells.virtual.euler.virtual_bend_euler(
-    width=1, radius=30, angle=30, layer=kf.kcl.infos.WG
-).show()
