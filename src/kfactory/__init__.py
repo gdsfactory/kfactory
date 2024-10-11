c0b11b4dec03a621fc01b40f8ce81408ffc781fb
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2024-10-12 01:38:43 +0200

    add deprecation error

--- src/kfactory/__init__.py
+++ src/kfactory/__init__.py
@@ -49,6 +49,7 @@ from . import (
 from .conf import config, logger
 from .enclosure import LayerEnclosure, KCellEnclosure
 from .grid import flexgrid_dbu, flexgrid, grid_dbu, grid
+import sys
 
 from aenum import constant  # type: ignore[import-untyped,unused-ignore]
 
@@ -97,3 +98,9 @@ __all__ = [
     "kf_types",
     "utils",
 ]
+_vi = sys.version_info
+if _vi.major < 3 or _vi.minor < 11:
+    logger.error(
+        "kfactory will not support python 3.10 in the future anymore. "
+        "Please upgrade to python >=3.11",
+    )
