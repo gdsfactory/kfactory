41514d8488f350979c2eaa4ae5e71c3292f1c2d3
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2024-10-12 02:11:11 +0200

    remove logger error

--- src/kfactory/__init__.py
+++ src/kfactory/__init__.py
@@ -49,7 +49,6 @@ from . import (
 from .conf import config, logger
 from .enclosure import LayerEnclosure, KCellEnclosure
 from .grid import flexgrid_dbu, flexgrid, grid_dbu, grid
-import sys
 
 from aenum import constant  # type: ignore[import-untyped,unused-ignore]
 
@@ -98,9 +97,3 @@ __all__ = [
     "kf_types",
     "utils",
 ]
-_vi = sys.version_info
-if _vi.major < 3 or _vi.minor < 11:
-    logger.error(
-        "kfactory will not support python 3.10 in the future anymore. "
-        "Please upgrade to python >=3.11",
-    )
