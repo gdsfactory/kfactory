6f3e6b2fa6c6c00a8b8e6e3c59b7aeb414fe0c05
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2025-02-12 01:46:31 +0100

    Bump to 1.1.1

--- src/kfactory/__init__.py
+++ src/kfactory/__init__.py
@@ -6,7 +6,7 @@ Uses the klayout package as a backend.
 # The import order matters, we need to first import the important stuff.
 # isort:skip_file
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 import klayout.db as kdb
 import klayout.lay as lay
