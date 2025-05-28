2561810659b5487b097d78c20a4b7bddc88ca9be
Inigo Perez Gamiz <iperezgamiz@psiquantum.com>
2025-05-28 16:59:18 -0700

    complete pins.py

--- src/kfactory/pins.py
+++ src/kfactory/pins.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
-from abc import abstractmethod
-from typing import TYPE_CHECKING, Any, Literal, Self, overload
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, ClassVar, Literal, Protocol, Self, overload
 
+from . import kdb
 from .conf import config
 from .pin import (
     BasePin,
+    DPin,
+    Pin,
     ProtoPin,
     filter_directions,
     filter_layer,
@@ -52,7 +55,7 @@ def _filter_pins(
     return list(pins)
 
 
-class ProtoPins:
+class ProtoPins(Protocol[TUnit]):
     """Base class for kf.Pins, kf.DPins."""
 
     _kcl: KCLayout
@@ -232,7 +235,333 @@ class ProtoPins:
         return hash(self._bases)
 
 
-class Pins: ...
+class ICreatePin(ABC):
+    """Protocol for a create_pin functionality"""
+
+    @property
+    @abstractmethod
+    def kcl(self) -> KCLayout: ...
+
+    @overload
+    def create_pin(
+        self,
+        *,
+        width: int | None = None,
+        layer: LayerEnum | int | None = None,
+        pin_type: str = "DC",
+        pos: kdb.Point | None = None,
+        allowed_angles: list[int] | None = None,
+        name: str | None = None,
+    ) -> Pin: ...
+
+    @overload
+    def create_pin(
+        self,
+        *,
+        width: int | None = None,
+        layer_info: kdb.LayerInfo | None = None,
+        pin_type: str = "DC",
+        pos: kdb.Point | None = None,
+        allowed_angles: list[int] | None = None,
+        name: str | None = None,
+    ) -> Pin: ...
+
+    def create_pin(
+        self,
+        *,
+        name: str | None = None,
+        width: int | None = None,
+        layer: LayerEnum | int | None = None,
+        layer_info: kdb.LayerInfo | None = None,
+        pin_type: str = "optical",
+        pos: kdb.Point | None = None,
+        allowed_angles: list[int] | None = None,
+    ) -> Pin:
+        """Create a pin."""
+
+        if width is None:
+            raise ValueError("width must be set.")
+        if allowed_angles is None:
+            raise ValueError("allowed angles must be set.")
+        if layer_info is None:
+            if layer is None:
+                raise ValueError("layer or layer_info must be defined to create a pin.")
+            layer_info = self.kcl.layout.get_info(layer)
+        assert layer_info is not None
+        if pos is None:
+            raise ValueError("pos must be set.")
+        pin = Pin(
+            name=name,
+            width=width,
+            layer_info=layer_info,
+            pin_type=pin_type,
+            allowed_angles=allowed_angles,
+            pos=pos,
+            kcl=self.kcl,
+        )
+
+        return self.add_pin(pin=pin)
+
+    @abstractmethod
+    def add_pin(
+        self,
+        *,
+        pin: ProtoPin[Any],
+        name: str | None = None,
+    ) -> Pin: ...
 
 
-class DPins: ...
+class DCreatePin(ABC):
+    """Protocol for a create_pin functionality"""
+
+    @property
+    @abstractmethod
+    def kcl(self) -> KCLayout: ...
+
+    @overload
+    def create_pin(
+        self,
+        *,
+        width: float | None = None,
+        layer: LayerEnum | int | None = None,
+        pin_type: str = "DC",
+        pos: kdb.Point | None = None,
+        allowed_orientations: list[float] | None = None,
+        name: str | None = None,
+    ) -> DPin: ...
+
+    @overload
+    def create_pin(
+        self,
+        *,
+        width: float | None = None,
+        layer_info: kdb.LayerInfo | None = None,
+        pin_type: str = "DC",
+        pos: kdb.Point | None = None,
+        allowed_orientations: list[float] | None = None,
+        name: str | None = None,
+    ) -> DPin: ...
+
+    def create_pin(
+        self,
+        *,
+        name: str | None = None,
+        width: float | None = None,
+        layer: LayerEnum | int | None = None,
+        layer_info: kdb.LayerInfo | None = None,
+        pin_type: str = "optical",
+        pos: kdb.Point | None = None,
+        allowed_orientations: list[float] | None = None,
+    ) -> DPin:
+        """Create a pin."""
+
+        if width is None:
+            raise ValueError("width must be set.")
+        if allowed_orientations is None:
+            raise ValueError("allowed angles must be set.")
+        if layer_info is None:
+            if layer is None:
+                raise ValueError("layer or layer_info must be defined to create a pin.")
+            layer_info = self.kcl.layout.get_info(layer)
+        assert layer_info is not None
+        if pos is None:
+            raise ValueError("pos must be set.")
+        dpin = DPin(
+            name=name,
+            width=width,
+            layer_info=layer_info,
+            pin_type=pin_type,
+            allowed_angles=allowed_orientations,
+            pos=pos,
+            kcl=self.kcl,
+        )
+
+        return self.add_pin(pin=dpin)
+
+    @abstractmethod
+    def add_pin(
+        self,
+        *,
+        pin: ProtoPin[Any],
+        name: str | None = None,
+    ) -> DPin: ...
+
+
+class Pins(ProtoPins[int], ICreatePin):
+    """A collection of dbu pins.
+
+    It is not a traditional dictionary. Elements can be retrieved as in a traditional
+    dictionary. But to keep tabs on names etc, the pins are stored as a list
+    """
+
+    yaml_tag: ClassVar[str] = "!Pins"
+
+    def __iter__(self) -> Iterator[Pin]:
+        """Iterator, that allows for loops etc to directly access the object."""
+        yield from (Pin(base=b) for b in self._bases)
+
+    def add_pin(
+        self,
+        *,
+        pin: ProtoPin[Any],
+        name: str | None = None,
+    ) -> Pin:
+        """Add a pin object.
+
+        Args:
+            pin: The pin to add
+            name: Overwrite the name of the pin
+        """
+        base = pin.base.model_copy()
+        if name is not None:
+            base.name = name
+        if pin.kcl != self.kcl:
+            base.kcl = self.kcl
+        self._bases.append(base)
+
+        return Pin(base=base)
+
+    def get_all_named(self) -> Mapping[str, Pin]:
+        """Get all pins in a dictionary with names as keys."""
+        return {v.name: Pin(base=v) for v in self._bases if v.name is not None}
+
+    def __getitem__(self, key: int | str | None) -> Pin:
+        """Get a specific pin by name."""
+        if isinstance(key, int):
+            return Pin(base=self._bases[key])
+        try:
+            return Pin(base=next(filter(lambda base: base.name == key, self._bases)))
+        except StopIteration as e:
+            raise KeyError(
+                f"{key=} is not a valid pin name or index. "
+                f"Available pins: {[v.name for v in self._bases]}"
+            ) from e
+
+    def copy(
+        self, rename_function: Callable[[Sequence[Pin]], None] | None = None
+    ) -> Self:
+        """Get a copy of each pin."""
+        bases = [b.__copy__() for b in self._bases]
+        if rename_function is not None:
+            rename_function([Pin(base=b) for b in bases])
+        return self.__class__(bases=bases, kcl=self.kcl)
+
+    def filter(
+        self,
+        allowed_angles: list[int] | None = None,
+        allowed_orientations: list[float] | None = None,
+        layer: LayerEnum | int | None = None,
+        pin_type: str | None = None,
+        regex: str | None = None,
+    ) -> Sequence[Pin]:
+        """Filter pins.
+
+        Args:
+            allowed_angles: Filter by allowed angles. 0, 1, 2, 3.
+            allowed_orientations: Filter by allowed orientations in degrees.
+            layer: Filter by layer.
+            pin_type: Filter by pin type.
+            regex: Filter by regex of the name.
+        """
+        return _filter_pins(
+            (Pin(base=b) for b in self._bases),
+            allowed_angles,
+            allowed_orientations,
+            layer,
+            pin_type,
+            regex,
+        )
+
+    def __repr__(self) -> str:
+        """Representation of the Pins as strings."""
+        return repr([repr(Pin(base=b)) for b in self._bases])
+
+
+class DPins(ProtoPins[float], DCreatePin):
+    """A collection of um pins.
+
+    It is not a traditional dictionary. Elements can be retrieved as in a traditional
+    dictionary. But to keep tabs on names etc, the pins are stored as a list
+    """
+
+    yaml_tag: ClassVar[str] = "!DPins"
+
+    def __iter__(self) -> Iterator[DPin]:
+        """Iterator, that allows for loops etc to directly access the object."""
+        yield from (DPin(base=b) for b in self._bases)
+
+    def add_pin(
+        self,
+        *,
+        pin: ProtoPin[Any],
+        name: str | None = None,
+    ) -> DPin:
+        """Add a pin object.
+
+        Args:
+            pin: The pin to add
+            name: Overwrite the name of the pin
+        """
+        base = pin.base.model_copy()
+        if name is not None:
+            base.name = name
+        if pin.kcl != self.kcl:
+            base.kcl = self.kcl
+        self._bases.append(base)
+
+        return DPin(base=base)
+
+    def get_all_named(self) -> Mapping[str, DPin]:
+        """Get all pins in a dictionary with names as keys."""
+        return {v.name: DPin(base=v) for v in self._bases if v.name is not None}
+
+    def __getitem__(self, key: int | str | None) -> DPin:
+        """Get a specific pin by name."""
+        if isinstance(key, int):
+            return DPin(base=self._bases[key])
+        try:
+            return DPin(base=next(filter(lambda base: base.name == key, self._bases)))
+        except StopIteration as e:
+            raise KeyError(
+                f"{key=} is not a valid pin name or index. "
+                f"Available pins: {[v.name for v in self._bases]}"
+            ) from e
+
+    def copy(
+        self, rename_function: Callable[[Sequence[DPin]], None] | None = None
+    ) -> Self:
+        """Get a copy of each pin."""
+        bases = [b.__copy__() for b in self._bases]
+        if rename_function is not None:
+            rename_function([DPin(base=b) for b in bases])
+        return self.__class__(bases=bases, kcl=self.kcl)
+
+    def filter(
+        self,
+        allowed_angles: list[int] | None = None,
+        allowed_orientations: list[float] | None = None,
+        layer: LayerEnum | int | None = None,
+        pin_type: str | None = None,
+        regex: str | None = None,
+    ) -> Sequence[DPin]:
+        """Filter pins by name.
+
+        Args:
+            allowed_angles: Filter by allowed angles. 0, 1, 2, 3.
+            allowed_orientations: Filter by allowed orientations in degrees.
+            layer: Filter by layer.
+            pin_type: Filter by pin type.
+            regex: Filter by regex of the name.
+        """
+        return _filter_pins(
+            (DPin(base=b) for b in self._bases),
+            allowed_angles,
+            allowed_orientations,
+            layer,
+            pin_type,
+            regex,
+        )
+
+    def __repr__(self) -> str:
+        """Representation of the Pins as strings."""
+        return repr([repr(Pin(base=b)) for b in self._bases])
