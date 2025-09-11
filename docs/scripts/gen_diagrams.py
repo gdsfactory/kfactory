43b203f8b30850d1d2a99913b5d38941a589a52c
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2025-09-12 00:29:12 +0800

    move scripts to scripts and execute the schematic generation in mkdocs

--- docs/scripts/gen_diagrams.py
+++ docs/scripts/gen_diagrams.py
@@ -13,10 +13,14 @@ class Schematic(kf.schematic.TSchematic[int]):
     __doc__ = kf.Schematic.__doc__
 
 
-_f = Path(__file__).parent / "source/_static"
+print("Generating Schematic and DSchematic diagrams...")
+
+_f = Path(__file__).parent.parent / "source/_static"
 diagram_dbu = erd.create(Schematic, terminal_models=[kf.KCLayout])
 diagram_dbu.models["kfactory.layout.KCLayout"].fields = {}
 diagram_dbu.draw(_f / "schematic.svg")
 diagram_um = erd.create(DSchematic, terminal_models=[kf.KCLayout])
 diagram_um.models["kfactory.layout.KCLayout"].fields = {}
 diagram_um.draw(_f / "dschematic.svg")
+
+print("Generated Schematic and DSchematic diagrams!")
