010b684ff9135e2d3de3365ee05a681a37674c96
Kevin Guan <kevinwguan@protonmail.com>
2025-09-12 01:10:05 +0000

    added labels flag to flatten
    Signed-off-by: Kevin Guan <kevinwguan@protonmail.com>

--- src/kfactory/kcell.py
+++ src/kfactory/kcell.py
@@ -1097,7 +1097,7 @@ class ProtoTKCell(ProtoKCell[TUnit, TKCell], Generic[TUnit], ABC):
         else:
             rename_func(self.ports)
 
-    def flatten(self, merge: bool = True) -> None:
+    def flatten(self, merge: bool = True, labels: bool = True) -> None:
         """Flatten the cell.
 
         Args:
@@ -1117,7 +1117,8 @@ class ProtoTKCell(ProtoKCell[TUnit, TKCell], Generic[TUnit], ABC):
                 texts = kdb.Texts(self.shapes(layer))
                 self.kdb_cell.clear(layer)
                 self.shapes(layer).insert(reg)
-                self.shapes(layer).insert(texts)
+                if labels:
+                    self.shapes(layer).insert(texts)
 
     def convert_to_static(self, recursive: bool = True) -> None:
         """Convert the KCell to a static cell if it is pdk KCell."""
